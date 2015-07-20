ab89f93bbae13e0d54b4ffa051af07504af6bf08
Leask Huang <i@leaskh.com>
2015-07-21 03:16:56 +0800

    [22.7346567625748,114.225352490886] Frozen @ Tue Jul 21 03:16:56 HKT 2015

---
+++ config.sample.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var config = {
+
+    port : 8069,
+
+    build_script : '*******.sh',
+
+    token : '****************************************************************',
+
+    mailgun : {
+        api_key    : '*******',
+        sender     : '*******@*******.com',
+        recipients : ['******@******.com']
+    }
+
+};
+
+module.exports = config;
--- main.js
+++ main.js
@@ -1,13 +1,67 @@
 'use strict';
 
+// load configs
+var config = require('./config');
+if (!config) {
+    console.log('Error loading configs!');
+    process.exit(1);
+}
+
+// import dependencies
 var express    = require('express'),
     http       = require('http'),
     bodyParser = require('body-parser'),
+    spawn      = require('child_process').spawn,
+    mailgun    = require('mailgun').Mailgun;
 
+// init express
 var bbApp = express();
-wdApp.server = http.createServer(wdApp);
+bbApp.server = http.createServer(bbApp);
 
-wdApp.all('*', function(req, res, next) {
-    console.log(req);
+// keep reference to config
+bbApp.config = config;
 
+// main
+var building = false;
+var report = new mailgun(bbApp.config.mailgun.api_key);
+
+bbApp.all('*', function(req, res, next) {
+    if (req.query.token !== 'bbApp.config.token') {
+        return res.send(401);
+    }
+    if (building) {
+        return res.send(200);
+    }
+    building = true;
+    var stdout = '';
+    var stderr = '';
+    var build  = spawn('sh', [bbApp.config.build_script]);
+    build.stdout.on('data', function(data) {
+        stdout += data;
+    });
+    build.stderr.on('data', function(data) {
+        stderr += data;
+    });
+    build.on('close', function(code) {
+        if (code) {
+            return res.send(500);
+        }
+        // console.log('child process exited with code ' + code);
+        report.sendText(
+            bbApp.config.mailgun.sender,
+            bbApp.config.mailgun.recipients,
+            'BitBot: Building Logs @ ' + new Date(),
+            stdout + '\n' + stderr,
+            function(err) {
+                err && console.log(err);
+                res.send(200);
+            }
+        );
+    });
+    building = false;
+});
+
+bbApp.listen(bbApp.config.port, function() {
+    console.log('<<<<<<< BitBot is now ON AIR! >>>>>>>');
+    console.log('listening @ ' + bbApp.config.port);
 });
